ffcfec4ee81cdcdb0b777056836ed8f7b61b483d
alanpinheiro96 <alantnt15@gmail.com>
2025-05-28 20:33:49 -0300

    Aplica efeito de AutoScroll

--- galloflix/src/app/services/movie-api.service.ts
+++ galloflix/src/app/services/movie-api.service.ts
@@ -22,4 +22,27 @@ export class MovieApiService {
   bannerApiData(): Observable<any> {
     return this.http.get(`${this.baseUrl}/trending/all/week?language=pt-br`, this.options);
   }
+
+  // Filmes em Destaque do Dia
+  trendingMovieApiData(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/trending/movie/day?language=pt-br`, this.options)
+  }
+
+  // Séries em Destaque do Dia
+  trendingSerieApiData(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/trending/tv/day?language=pt-br`, this.options)
+  }
+
+  // Filmes de ação mais populares
+  popularActionMovieApiData(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/discover/movie?language=pt-br&with_genres=28&sort_by=popularity.desc`, this.options)
+  }
+  // Filmes de ação mais populares
+  trendingAnimationMovieApiData(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/discover/movie?language=pt-br&with_genres=16&sort_by=popularity.desc`, this.options)
+  }
+  // Filmes de ação mais populares
+  trendingAnimationSerieApiData(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/discover/tv?language=pt-br&with_genres=16&sort_by=popularity.desc`, this.options)
+  }
 }
