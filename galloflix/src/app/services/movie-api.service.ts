80b5c02b1e42dbe547eef0e95bf9407e8f7c1844
alanpinheiro96 <alantnt15@gmail.com>
2025-06-04 20:26:53 -0300

    Cria pagina de detalhes

--- galloflix/src/app/services/movie-api.service.ts
+++ galloflix/src/app/services/movie-api.service.ts
@@ -37,12 +37,29 @@ export class MovieApiService {
   popularActionMovieApiData(): Observable<any> {
     return this.http.get(`${this.baseUrl}/discover/movie?language=pt-br&with_genres=28&sort_by=popularity.desc`, this.options)
   }
+
   // Filmes de ação mais populares
   trendingAnimationMovieApiData(): Observable<any> {
     return this.http.get(`${this.baseUrl}/discover/movie?language=pt-br&with_genres=16&sort_by=popularity.desc`, this.options)
   }
+
   // Filmes de ação mais populares
   trendingAnimationSerieApiData(): Observable<any> {
     return this.http.get(`${this.baseUrl}/discover/tv?language=pt-br&with_genres=16&sort_by=popularity.desc`, this.options)
+  }
+
+  // Detalhes do Filme ou Série
+  mediaDetails(type: any, value: any): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${type}/${value}?language=pt-br`, this.options);
+  }
+
+  // Trailers do Filme ou Série
+  mediaTrailers(type: any, value: any): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${type}/${value}/videos?language=pt-br`, this.options);
+  }
+ 
+  // Elenco do Filme ou Série
+  mediaCast(type: any, value: any): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${type}/${value}/credits?language=pt-br`, this.options);
   }
 }
